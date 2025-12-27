b2d98aa8e65ce34d5af1ccfa7f5ead3a9fd3f1b5
barbarastefany <barbara.stefany@outlook.com.br>
2025-12-27 14:09:04 -0300

    Initial commit

---
+++ src/main/java/com/barbara/workflowtests/WorkflowtestsApplication.java
@@ -0,0 +1,13 @@
+package com.barbara.workflowtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowtestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowtestsApplication.class, args);
+	}
+
+}
